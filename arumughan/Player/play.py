3134880ae688f964f73b95d8486fb370277c682d
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-07-07 15:09:21 +0530

    Update play.py

--- arumughan/Player/play.py
+++ arumughan/Player/play.py
@@ -249,7 +249,7 @@ async def play(c: Client, m: Message):
                    )
                 await add_active_chat(chat_id)
                 add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-        # else:
+            else:
                 suhu = await m.reply_text(f"**Downloading**\n\n0% ▓▓▓▓▓▓▓▓▓▓▓▓ 100%")
                 query = m.text.split(None, 1)[1]
                 search = ytsearch(query)
